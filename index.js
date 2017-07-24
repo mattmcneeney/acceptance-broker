24674ab45398ebc9fa18e05f066c341cb5bfed46
Matt McNeeney <mmcneeney@pivotal.io>
2017-07-24 09:42:20 +0100

    3 space tabs

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+app = express(),
+bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/v2/catalog', function(req, res) {
-    res.json({
-        services: [{
-            name: 'acceptance-broker',
-            description: 'A simple service broker used for acceptance',
-            id: '011dc653-871b-4bae-aff2-90bff0c75ca2',
-            bindable: true,
-            plans: [{
-                name: 'standard',
-                id: '97c8c167-de95-43c7-93df-12ee82059c0b',
-                description: 'standard plan',
-                free: true,
-                schemas: {
-                    service_instance: {
-                        create: {
-                            parameters: {
-                                $schema: 'http://json-schema.org/draft-04/schema#',
-                                type: 'object',
-                                properties: {
-                                    test: {
-                                        type: 'notatype',
-                                        default: 10
-                                    }
-                                }
-                            }
+   res.json({
+      services: [{
+         name: 'acceptance-broker',
+         description: 'A simple service broker used for acceptance',
+         id: '011dc653-871b-4bae-aff2-90bff0c75ca2',
+         bindable: true,
+         plans: [{
+            name: 'standard',
+            id: '97c8c167-de95-43c7-93df-12ee82059c0b',
+            description: 'standard plan',
+            free: true,
+            schemas: {
+               service_instance: {
+                  create: {
+                     parameters: {
+                        $schema: 'http://json-schema.org/draft-04/schema#',
+                        type: 'object',
+                        properties: {
+                           test: {
+                              type: 'notatype',
+                              default: 10
+                           }
                         }
-                    }
-                }
-            }]
-        }]
-    });
+                     }
+                  }
+               }
+            }
+         }]
+      }]
+   });
 });
 app.put('/v2/service_instances/:instance_id', function(req, res) {
-    res.json({});
+   res.json({});
 });
 app.patch('/v2/service_instances/:instance_id', function(req, res) {
-    res.json({});
+   res.json({});
 });
 app.delete('/v2/service_instances/:instance_id', function(req, res) {
-    res.json({});
+   res.json({});
 });
 app.put('/v2/service_instances/:instance_id/service_bindings/:binding_id', function(req, res) {
-    res.json({});
+   res.json({});
 });
 app.delete('/v2/service_instances/:instance_id/service_bindings/:binding_id', function(req, res) {
-    res.json({});
+   res.json({});
 });
 
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
-    console.log('Broker running on port ' + server.address().port);
+   console.log('Broker running on port ' + server.address().port);
 });
